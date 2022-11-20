da49cc213f67de51e794361f2796b06680e79c34
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-11-20 11:43:30 -0300

    teste novo deploy

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send({ message: "Deu certo papaaayyy" })
+  res.status(200).send({ message: "Deu certo papaaayyy, novo test" })
 })
 
 app.listen(3000, () => console.log("SERVER IS RUNNING AT http://localhost:3000"));
