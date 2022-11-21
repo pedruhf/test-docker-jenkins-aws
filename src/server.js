30c67d7b8be060cf44481ed44c385495762efd6c
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-11-20 22:59:40 -0300

    release 1.0.2

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send({ message: "Deu certo papaaayyy, novo test" })
+  res.status(200).send({ message: "Deu certo papaaayyy, nova versao" })
 })
 
 app.listen(3000, () => console.log("SERVER IS RUNNING AT http://localhost:3000"));
